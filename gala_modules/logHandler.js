953ee285c853b0c62dfe86905950a53706ffe89b
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2019-08-09 23:01:14 -0400

    Fixed circular structure error

--- gala_modules/logHandler.js
+++ gala_modules/logHandler.js
@@ -1,26 +1,32 @@
 /**
- * Copyright 2019 Jordan Owens
+ * Copyright 2019 Galaktikos
  * All rights reserved.
  */
 
-const logTypes = ["channelCreate", "channelDelete", "channelPinsUpdate", "channelUpdate", "clientUserGuildSettingsUpdate", "clientUserSettingsUpdate", "debug", "disconnect", "emojiCreate", "emojiDelete", "emojiUpdate", "error", "guildBanAdd", "guildBanRemove", "guildCreate", "guildDelete", "guildIntegrationsUpdate", "guildMemberAdd", "guildMemberAvailable", "guildMemberRemove", "guildMembersChunk", "guildMemberSpeaking", "guildMemberUpdate", "guildUnavailable", "guildUpdate", "message", "messageDelete", "messageDeleteBulk", "messageReactionAdd", "messageReactionRemove", "messageReactionRemoveAll", "messageUpdate", "presenceUpdate", "rateLimit", "ready", "reconnecting", "resume", "roleCreate", "roleDelete", "roleUpdate", "typingStart", "typingStop", "userNoteUpdate", "userUpdate", "voiceStateUpdate", "warn", "webhookUpdate"];
+const logTypes = ["channelCreate", "channelDelete", "channelPinsUpdate", "channelUpdate", "clientUserGuildSettingsUpdate", "clientUserSettingsUpdate", "debug", "disconnect", "emojiCreate", "emojiDelete", "emojiUpdate", "error", "guildBanAdd", "guildBanRemove", "guildCreate", "guildDelete", "guildIntegrationsUpdate", "guildMemberAdd", "guildMemberAvailable", "guildMemberRemove", "guildMembersChunk", "guildMemberSpeaking", "guildMemberUpdate", "guildUnavailable", "guildUpdate", "message", "messageDelete", "messageDeleteBulk", "messageReactionAdd", "messageReactionRemove", "messageReactionRemoveAll", "messageUpdate", "presenceUpdate", "rateLimit", "ready", "reconnecting", "resume", "roleCreate", "roleDelete", "roleUpdate", "typingStart", "typingStop", "userNoteUpdate", "userUpdate", "voiceStateUpdate", "warn", "webhookUpdate"],
+    fs = require("fs"),
+    CircularJSON = require("circular-json");
 let modules, logs = [];
 module.exports = {
     initialize(e) {
         modules = e
     },
     start(e) {
-        logTypes.forEach(l => {
-            e.on(l, e => {
-                let a = l;
+        fs.readFile("gala_logs.json", (e, a) => {
+            e ? modules.errorHandler.readError("gala_logs.json") : logs = JSON.parse(a)
+        }), logTypes.forEach(a => {
+            e.on(a, e => {
+                let l = a;
                 logs.push({
-                    type: a,
+                    type: l,
                     time: Date.now(),
                     info: e
                 })
             })
         }), setInterval(() => {
-            logFile = modules.fileHandler.read("gala_logs.json"), logFile ? modules.fileHandler.write("gala_logs.json", logFile.push(logs)) : modules.fileHandler.write("gala_logs.json", logs)
+            fs.writeFile("gala_logs.json", CircularJSON.stringify(logs), e => {
+                e && modules.errorHandler.writeError("gala_logs.json")
+            })
         }, 5e3)
     }
 };
